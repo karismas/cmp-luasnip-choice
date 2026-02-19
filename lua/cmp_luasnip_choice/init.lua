2526a038469a9f08458456f1279ea25155392f50
Samuel Huber <hubers001@gmail.com>
2026-02-19 13:49:27 -0500

    updating

--- lua/cmp_luasnip_choice/init.lua
+++ lua/cmp_luasnip_choice/init.lua
@@ -1,78 +1,74 @@
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 local default_config = {
-    auto_open = true,
+	auto_open = true,
 }
 
 local M = {
-    source = {}
+	source = {},
 }
 
 function M.setup(config)
-    if config == nil then
-        config = default_config
-    else
-        config = vim.tbl_deep_extend('keep', config, default_config)
-    end
+	if config == nil then
+		config = default_config
+	else
+		config = vim.tbl_deep_extend("keep", config, default_config)
+	end
 
-    if config.auto_open then
-        vim.api.nvim_create_autocmd('User', {
-            pattern = 'LuasnipChoiceNodeEnter',
-            callback = function()
-                vim.schedule(function()
-                    cmp.complete({
-                        config = {
-                            sources = {
-                                {name = 'luasnip_choice'}
-                            },
-                        },
-                    })
-                end)
-            end,
-        })
-    end
+	if config.auto_open then
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LuasnipChoiceNodeEnter",
+			callback = function()
+				vim.schedule(function()
+					cmp.complete({
+						config = {
+							sources = {
+								{ name = "luasnip_choice" },
+							},
+						},
+					})
+				end)
+			end,
+		})
+	end
 end
 
 function M.source.new()
-    return setmetatable({}, {__index = M.source})
+	return setmetatable({}, { __index = M.source })
 end
 
 function M.source:is_available()
-    return require('luasnip.session').active_choice_node
+	return require("luasnip.session").active_choice_node
 end
 
 function M.source:execute(completion_item, callback)
-    require('luasnip').set_choice(completion_item.index)
-    callback(completion_item)
+	require("luasnip").set_choice(completion_item.index)
+	callback(completion_item)
 end
 
 function M.source:get_keyword_pattern()
-    return ''
+	return ""
 end
 
 function M.source:complete(_, callback)
-    local items = {}
+	local items = {}
+	local choices = require("luasnip.session").active_choice_nodes[1].choices
+	for _, choice in ipairs(choices) do
+		local label = choice:get_static_text()
+		table.insert(items, {
+			label = label,
+			word = label,
+			index = _,
+			documentation = label,
+			kind = cmp.lsp.CompletionItemKind.Snippet,
+		})
+	end
 
-    local choices_ok, choice_docstrings = pcall(require('luasnip').get_current_choices)
-    if not choices_ok then
-        -- no choice active: return no completion-items.
-        callback(items)
-    end
-
-    for _, choice_docstring in ipairs(choice_docstrings) do
-        table.insert(items, {
-            label = choice_docstring,
-            word = "",
-            index = _,
-            documentation = choice_docstring,
-            kind = cmp.lsp.CompletionItemKind.Snippet,
-        })
-    end
-    callback(items)
+	callback(items)
 end
 
 function M.source:get_debug_name()
-    return 'luasnip_choice'
+	return "luasnip_choice"
 end
 
 return M
